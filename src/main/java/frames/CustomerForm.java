46f32105368c7297b869a5dea343f48a6f676622
David Alonso Aguilar <daa0010@alu.medac.es>
2026-05-06 12:55:05 +0200

    Ventana cliente v1

--- src/main/java/frames/CustomerForm.java
+++ src/main/java/frames/CustomerForm.java
@@ -6,4 +6,14 @@ public class CustomerForm {
     private JPanel customerPanel;
     private JTable customerTable;
     private JButton btnAddCustomer;
+    private JLabel DNI;
+    private JTextField txtDni;
+    private JTextField txtName;
+    private JTextField txtSurname;
+    private JTextField txtEmail;
+    private JTextField txtPhone;
+
+    public JPanel getCustomerPanel() {
+        return customerPanel;
+    }
 }
